2518a3ef6590ea398f61c88a5d95e675e880ba8c
Neda Keshavarzi <neda.keshavarz81@gmail.com>
2021-11-11 17:59:57 +0000

    Add generating product_id and calendar_id

--- sheet.py
+++ sheet.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pydantic import BaseModel
+import uuid
+from gcalendar import add_event
 
 SCOPES_SHEETS = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -16,19 +18,40 @@ products_sheet = SHEET.worksheet('products')
 
 
 class Product(BaseModel):
-    id: int
+    id: str
+    calendar_id: str
     title: str
     description: str
-    price: float
+    address: str
+    capacity: str
+    price: str
+    date: str
+    time: str
+    emails: str
 
 
 def add_product(product):
-     products_sheet.append_row(product)
+    product_values = list(product.dict().values())
+    products_sheet.append_row(product_values)
+    event = {
+        'id': product.calendar_id,
+        'summary': product.title,
+        'location': product.address,
+        'description': build_description(product.description, product.capacity, 0),
+        'start': {
+            'dateTime': convert_to_gcalendar_friendly_date(product.date,product.time),
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': convert_to_gcalendar_friendly_date(product.date, (datetime.strptime(product.time, '%H:%M:%S') + timedelta(minutes=45)).strftime('%H:%M:%S')),
+            'timeZone': 'America/Los_Angeles',
+        }
+    }
+    add_event(event)
 
 
-def main():
-    sample_product = Product(id=1, title='product 1', description="a product", price=0.1)
-    product_values = list(sample_product.dict().values())
-    add_product(product_values)
+def new_product_id():
+    return str(len(products_sheet.get_all_records()))
 
-main()
+def generate_calendar_id():
+    return str(uuid.uuid4()).replace('-', '')
