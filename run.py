4999b4bdacc1a123dc1e3548e2677e1fcc6e10d2
Neda Keshavarzi <neda.keshavarz81@gmail.com>
2021-11-12 11:32:12 +0000

    Add show proucts when userchooses booking

--- run.py
+++ run.py
@@ -28,3 +28,34 @@ def splash():
                 print('Product saved')
             return False
         print('You need to press "a" or "b"')
+
+
+def show_products():
+    print('Following is the list of products you can choose from:\n')
+    products = list_products()
+    formatted_products_to_choose = ''
+    for product_row in products:
+        formatted_products_to_choose += product_row.product.id + ': ' + product_row.product.title + '  '
+    print(formatted_products_to_choose)
+    chosen_product = input(
+        'Choose the number in front of the product to book:\n')
+    selected_product = list(filter(lambda p: p.product.id == chosen_product, products))[0].product
+
+    print(
+        f'{selected_product.title} is available on {selected_product.date} at {selected_product.time}')
+
+    chosen_option = input(
+        '(a): continue. (p): product menu, (b):welcome screen\n')
+    if chosen_option == 'a':
+        customer_email = input('Enter your email address to book:\n')
+        # validate email
+        add_booking(chosen_product, customer_email)
+        print(
+            f'You are booked for {selected_product.title} on {selected_product.date} at {selected_product.time}')
+    if chosen_option == 'p':
+        show_products()
+    if chosen_option == 'b':
+        splash()
+
+
+splash()
