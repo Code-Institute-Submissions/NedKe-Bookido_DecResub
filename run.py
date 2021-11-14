414a9989ce38cbac1d3e8f2d108eb0c691bd7ddf
Neda Keshavarzi <neda.keshavarz81@gmail.com>
2021-11-14 06:32:17 +0000

    Add product prompt  with vvalidation

--- run.py
+++ run.py
@@ -24,19 +24,84 @@ def welcome_screen():
         if chosen_option == 'a':
             password = input('Please enter password:\n')
             if password == 'password':
-                title = input('Enter product title:\n')
-                description = input('Enter product description:\n')
-                address = input('Enter product address:\n')
-                capacity = input('Enter product capaity:\n')
-                duration = input(
-                    '\nEnter product duration in minutes ("e" for welcome screen):\n')
-                price = input('Enter product price [0.2]:\n')
-                date = input('Enter product date [2021-11-11]:\n')
-                time = input('Enter product time [13:00:00]:\n')
-                add_product_raw(title, description, address, price, capacity, duration, date, time)
-                print('Product saved')
+                add_product_prompt()
+
             return False
         print('You need to press "a" or "b"')
+
+
+def add_product_prompt():
+    """
+    Adds a product to the product catalog by asking the admin to add information
+    about that product
+    """
+    print("Adding a new product. Please answer following questions:\n")
+    title = input('Enter product title (Press "e" for welcome screen):\n')
+    exit_to_main_screen(title)
+    description = input(
+        '\nEnter product description ("e" for welcome screen):\n')
+    exit_to_main_screen(description)
+    address = input('\nEnter product address ("e" for welcome screen):\n')
+    exit_to_main_screen(address)
+    capacity = input('\nEnter product capacity ("e" for welcome screen):\n')
+    exit_to_main_screen(capacity)
+    duration = input(
+        '\nEnter product duration in minutes ("e" for welcome screen):\n')
+    exit_to_main_screen(duration)
+    price = input('\nEnter product price ("e" for welcome screen):\n')
+    exit_to_main_screen(price)
+
+    while True:
+        date = input(
+            f'Enter product date (example: '
+            f'{datetime.now().strftime("%Y-%m-%d")}) '
+            f'("e" for welcome screen):\n')
+        exit_to_main_screen(date)
+        if not re.match(r'\d{4}-\d{2}-\d{2}', date):
+            print(
+                f'Please enter date in the format as this example: '
+                f'{datetime.now().strftime("%Y-%m-%d")}\n')
+        if datetime.now().date() > datetime.strptime(date, '%Y-%m-%d').date():
+            print('Enter a date later or equal to today')
+        else:
+            break
+
+    while True:
+        time = input(
+            f'Enter product time (example: '
+            f'{datetime.now().strftime("%H:%M:%S")}) '
+            f'("e" for welcome screen):\n')
+        exit_to_main_screen(time)
+        if not re.match(r'\d{2}:\d{2}:\d{2}', date):
+            print('Please enter time in the format as this example: 13:00:00\n')
+        if datetime.now() >= datetime.strptime(
+            f'{date} {time}', '%Y-%m-%d %H:%M:%S'):
+            print('Enter a time that is later than current time\n')
+        else:
+            break
+
+    print(
+        f'\nConfirm adding {title} on {date} at {time} with price {price} €:')
+    confirm = input('(y): Yes, n: (No)\n')
+    if confirm.lower() == 'y':
+        print("Saving the product please wait ...")
+        add_product_raw(
+            title,
+            description,
+            address,
+            price,
+            capacity,
+            duration,
+            date,
+            time)
+        print('Product saved.')
+        input('\nPress any key to go to welcome screen: \n')
+        welcome_screen()
+    if confirm.lower() == 'n':
+        chosen_option = input('(1): Add new product, (e): main screen')
+        exit_to_main_screen(chosen_option)
+        if chosen_option == '1':
+            add_product_prompt()
 
 
 def customer_flow():
@@ -65,6 +130,24 @@ def customer_flow():
         show_products()
     if chosen_option == 'b':
         splash()
+
+
+def exit_to_main_screen(user_input):
+    """
+    Helper function to go to the main screen
+    :param user_input:
+    """
+    if user_input.lower() == 'e':
+        welcome_screen()
+
+
+def exit_to_product_menu(user_input):
+    """
+    Helper function to go to the product menu
+    :param user_input:
+    """
+    if user_input.lower() == 'p':
+        customer_flow()
 
 
 welcome_screen()
