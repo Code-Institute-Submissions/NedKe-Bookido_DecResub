6d660d51c81de47e950a6d91e325d835a96fcf4f
Neda Keshavarzi <neda.keshavarz81@gmail.com>
2021-11-14 06:30:25 +0000

    Format sheet.py

--- sheet.py
+++ sheet.py
@@ -1,3 +1,9 @@
+"""
+sheet module contains settings and methods to create new product and also
+add bookings to the already creating products
+"""
+
+
 from datetime import datetime, timedelta
 import uuid
 import gspread
@@ -5,16 +11,17 @@ from google.oauth2.service_account import Credentials
 from pydantic import BaseModel
 from gcalendar import add_event, update_event_description
 
+
 SCOPES_SHEETS = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive.file',
     'https://www.googleapis.com/auth/drive'
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPES_SHEETS)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('fartoor_products')
+SHEET = GSPREAD_CLIENT.open('bookido')
 products_sheet = SHEET.worksheet('products')
 
 GMT_OFF = '+01:00'
@@ -41,16 +48,25 @@ class ProductRow(BaseModel):
     product: Product
 
 
-class ProductNotFoundException(Exception):
+def convert_to_gcalendar_friendly_date(date, time):
     """
-    Exception to be thrown when a product not found in the list of products
+    Converts date and time parameters to Google calendar friendly format
+    :param date: date of an event
+    :param time: time of an event
     """
-    pass
+    return f'{date}T{time}{GMT_OFF}'
 
 
 class CapacityReachedException(Exception):
     """
     Exception to be thrown when capacity is reached while booking a product
+    """
+    pass
+
+
+class ProductNotFoundException(Exception):
+    """
+    Exception to be thrown when a product not found in the list of products
     """
     pass
 
@@ -70,6 +86,7 @@ def add_booking(product_id, email):
     :param product_id: product to be booked
     :param email: customer's email
     """
+
     product_row = get_product_row(product_id)
     product = product_row.product
     emails = product.emails.split(',') if product.emails != '' else []
@@ -80,21 +97,15 @@ def add_booking(product_id, email):
     emails.append(email)
 
     emails_to_store = email if len(emails) == 1 else ','.join(emails)
-    products_sheet.update_cell(product_row.row_num, 10, emails_to_store)
-    description = build_description(product.description, product.capacity, len(emails))
+    emails_row = 11
+    products_sheet.update_cell(
+        product_row.row_num,
+        emails_row,
+        emails_to_store)
+    description = build_description(product.description, product.capacity,
+                                    len(emails))
     description += '\n' + '\n'.join(emails_to_store.split(','))
     update_event_description(product.calendar_id, description)
-
-
-def get_product_row(product_id):
-    """
-    Returns the product found in the list of products using product_id
-    :param product_id
-    """
-    for product_row in list_products():
-        if product_row.product.id == product_id:
-            return product_row
-    raise ProductNotFoundException("product not found")
 
 
 def build_description(product_description, product_capacity, num_booked):
@@ -118,26 +129,27 @@ def add_product(product):
         'id': product.calendar_id,
         'summary': product.title,
         'location': product.address,
-        'description': build_description(product.description, product.capacity, 0),
+        'description': build_description(product.description, product.capacity,
+                                         0),
         'start': {
-            'dateTime': convert_to_gcalendar_friendly_date(product.date,product.time),
-            'timeZone': 'America/Los_Angeles',
+            'dateTime': convert_to_gcalendar_friendly_date(product.date,
+                                                           product.time),
+            'timeZone': 'Europe/Stockholm',
         },
         'end': {
-            'dateTime': convert_to_gcalendar_friendly_date(product.date, (datetime.strptime(product.time, '%H:%M:%S') + timedelta(minutes=45)).strftime('%H:%M:%S')),
-            'timeZone': 'America/Los_Angeles',
+            'dateTime': convert_to_gcalendar_friendly_date(product.date, (
+                datetime.strptime(product.time, '%H:%M:%S') + timedelta(
+                    minutes=45)).strftime('%H:%M:%S')),
+            'timeZone': 'Europe/Stockholm',
         }
     }
     add_event(event)
 
 
-def convert_to_gcalendar_friendly_date(date, time):
-    """
-    Converts date and time parameters to Google calendar friendly format
-    :param date: date of an event
-    :param time: time of an event
-    """
-    return f'{date}T{time}{GMT_OFF}'
+def generate_calendar_id():
+    """Generates Calendar id to for Google calendar events"""
+
+    return str(uuid.uuid4()).replace('-', '')
 
 
 def generate_new_product_id():
@@ -148,17 +160,51 @@ def generate_new_product_id():
     return str(len(products_sheet.get_all_records()))
 
 
-def generate_calendar_id():
-    """Generates Calendar id to for Google calendar events"""
-    return str(uuid.uuid4()).replace('-', '')
-
-
 def list_products():
+    """Deserializes all prducts inito ProductRow model"""
     products = products_sheet.get_all_records()
-    return [ProductRow(row_num=i+2, product=Product(id=p['id'], calendar_id=p['calendar_id'], title=p['title'], description=p['description'], address=p['address'], capacity=p['capacity'], duration=p['duration'], price=p['price'], date=p['date'], time=p['time'], emails=p['emails'])) for i, p in enumerate(products)]
+
+    product_rows = [ProductRow(row_num=i + 2,
+                               product=Product(id=p['id'],
+                                               calendar_id=p['calendar_id'],
+                                               title=p['title'],
+                                               description=p['description'],
+                                               address=p['address'],
+                                               capacity=p['capacity'],
+                                               duration=p['duration'],
+                                               price=p['price'],
+                                               date=p['date'], time=p['time'],
+                                               emails=p['emails'])) for i, p in
+                    enumerate(products)]
+
+    return list(
+        filter(
+            lambda p: datetime.strptime(
+                f'{p.product.date} {p.product.time}',
+                '%Y-%m-%d %H:%M:%S') > datetime.now(),
+            product_rows))
 
 
-def add_product_raw(title, description, address, price, capacity, duration, date, time):
+def get_product_row(product_id):
+    """
+    Returns the product found in the list of products using product_id
+    :param product_id
+    """
+    for product_row in list_products():
+        if product_row.product.id == product_id:
+            return product_row
+    raise ProductNotFoundException("product not found")
+
+
+def add_product_raw(
+        title,
+        description,
+        address,
+        price,
+        capacity,
+        duration,
+        date,
+        time):
     """
     Creates a new product model and stores it using add_product method
     :param title
@@ -172,5 +218,16 @@ def add_product_raw(title, description, address, price, capacity, duration, date
     """
     calendar_id = generate_calendar_id()
     product_id = generate_new_product_id()
-    new_product = Product(id=product_id, calendar_id=calendar_id, title=title, description=description, address=address, price=price, capacity=capacity, duration=duration, date=date, time=time, emails='')
-    add_product(sample_product)
+    new_product = Product(
+        id=product_id,
+        calendar_id=calendar_id,
+        title=title,
+        description=description,
+        address=address,
+        price=price,
+        duration=duration,
+        capacity=capacity,
+        date=date,
+        time=time,
+        emails='')
+    add_product(new_product)
